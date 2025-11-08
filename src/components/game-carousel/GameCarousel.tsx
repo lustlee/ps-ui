31918c530c9401df04c72b5dc9c802fef61aab2c
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-11-09 01:29:25 +0600

    feat: add hotkeys functionality for game navigation and integrate react-hotkeys-hook

--- src/components/game-carousel/GameCarousel.tsx
+++ src/components/game-carousel/GameCarousel.tsx
@@ -1,25 +1,41 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRef } from 'react';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 import { GAMES_DATA } from '../../data/games.data';
 import { GameItem } from './game-item/GameItem';
 
 import 'swiper/css';
 import { useGameStore } from '../../store';
+import { useHotkeys } from 'react-hotkeys-hook';
+import { HotKeysConfig } from '../../config/hotkeys.config';
 
 export const GameCarousel = () => {
-	const { activeGameSlug, setActiveGameSlug } = useGameStore();
+	const { setActiveGameSlug } = useGameStore();
+	const swiperRef = useRef<SwiperRef>(null);
+
+	useHotkeys(HotKeysConfig.NEXT_GAME, () => {
+		if (swiperRef.current) {
+			swiperRef.current?.swiper.slideNext();
+		}
+	});
+
+	useHotkeys(HotKeysConfig.PREV_GAME, () => {
+		if (swiperRef.current) {
+			swiperRef.current?.swiper.slidePrev();
+		}
+	});
 
 	return (
 		<div className='ml-32'>
 			<Swiper
+				ref={swiperRef}
 				slidesPerView={8}
 				spaceBetween={18}
 				loop
 				slideToClickedSlide
-				initialSlide={2}
+				initialSlide={0}
 				onSlideChange={(e) => {
 					const gameSlug = GAMES_DATA[e.realIndex].slug;
 					setActiveGameSlug(gameSlug);
-					console.log(gameSlug);
 				}}
 			>
 				{GAMES_DATA.map((game) => (
--- src/config/hotkeys.config.ts
+++ src/config/hotkeys.config.ts
@@ -1,6 +1,20 @@
+
+export const KeyEnum = {
+  ArrowUp: 'ArrowUp',
+  ArrowDown: 'ArrowDown',
+  ArrowLeft: 'ArrowLeft',
+  ArrowRight: 'ArrowRight',
+  Enter: 'Enter',
+  Space: ' ',
+  Tab: 'Tab',
+  Shift: "Shift",
+  Control: "Control",
+  Meta: "Meta",
+} as const;
+
 export class HotKeysConfig {
   static readonly TOP_MENU = '1';
-  static readonly DETAILS = '⬇';
-  static readonly NEXT_GAME = '➡';
-  static readonly PREV_GAME = '⬅';
+  static readonly DETAILS = KeyEnum.ArrowDown;
+  static readonly NEXT_GAME = KeyEnum.ArrowRight;
+  static readonly PREV_GAME = KeyEnum.ArrowLeft;
 }
