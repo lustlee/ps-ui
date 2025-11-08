b87600cc58dfe2ed0f5739d659bcc69974a2ede1
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-11-09 00:05:49 +0600

    feat: integrate Swiper for game carousel and enhance styles

--- src/components/game-carousel/GameCarousel.tsx
+++ src/components/game-carousel/GameCarousel.tsx
@@ -1,12 +1,19 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { GAMES_DATA } from '../../data/games.data';
 import { GameItem } from './game-item/GameItem';
 
+import 'swiper/css';
+
 export const GameCarousel = () => {
 	return (
-		<div className='flex items-center gap-2'>
-			{GAMES_DATA.map((game) => (
-				<GameItem key={game.slug} game={game} />
-			))}
+		<div className='ml-32'>
+			<Swiper slidesPerView={8} spaceBetween={18} loop slideToClickedSlide initialSlide={2}>
+				{GAMES_DATA.map((game) => (
+					<SwiperSlide key={game.slug}>
+						<GameItem game={game} />
+					</SwiperSlide>
+				))}
+			</Swiper>
 		</div>
 	);
 };
--- src/components/game-carousel/game-item/GameItem.tsx
+++ src/components/game-carousel/game-item/GameItem.tsx
@@ -9,15 +9,15 @@ interface Props {
 export const GameItem: FC<Props> = ({ game }) => {
 	return (
 		<button>
-			<div className={cn('p-1 border-2 border-transparent transition-colors duration-300 ease-in-out')}>
-				<img src={game.coverImage} alt={game.title} className='w-56 h-56 object-cover' />
+			<div className={cn('p-0.5 border-2 border-transparent transition-colors duration-300 ease-in-out')}>
+				<img src={game.coverImage} alt={game.title} className='w-44 h-44 object-cover transition-all duration-300 ease-in-out' />
 			</div>
 			<div className='mt-2'>
 				<img
 					src={game.platform === 'PS5' ? '/images/ps-5.png' : '/images/ps-4.png'}
 					alt={game.platform}
-					width={60}
-					className='filter brightness-0 invert opacity-50'
+					width={50}
+					className='filter brightness-0 invert fadeIn opacity-0 transition-opacity duration-300 ease-in-out'
 				/>
 			</div>
 		</button>
